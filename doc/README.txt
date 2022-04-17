04aeca0da71c0baa09d8bf3305fe64aab49f642d
Ernesting <ernesto.6.ds@gmail.com>
2022-04-17 12:54:09 +0200

    Practica3_2.0

---
+++ doc/README.txt
@@ -0,0 +1,6 @@
+Adrian Arribas Mateo 795593 y Ernesto Bielsa Gracia 798799
+Práctica 1: Construcción de un analizador léxico para "adac".
+
+Practica 2: Construccion de un analizador sintactico descendente para "adac" con
+	recuperacion de errores en modo panico, saltando tokens hasta llegar a
+	uno de los siguientes de sincronizacion segun el caso: ';', 'end', ')', ']'.
---
+++ lib/attributes/Attributes.java
@@ -0,0 +1,73 @@
+//*****************************************************************
+// File:   Attributes.java
+// Author: Procesadores de Lenguajes-University of Zaragoza
+// Date:   enero 2022
+//         Clase única para almacenar los atributos que pueden aparecer en el traductor de adac
+//         Se hace como clase plana, por simplicidad. Los atributos que se pueden almacenar
+//         se autoexplican con el nombre
+//*****************************************************************
+
+package lib.attributes;
+import lib.symbolTable.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.AbstractMap.SimpleEntry;
+
+public class Attributes implements Cloneable {
+    public Symbol.Types type;
+    public Symbol.ParameterClass parClass;
+    
+    public int valInt;
+    public boolean valBool;
+    public char valChar;
+    public String valString;
+    //COMPLETAR
+    public String code; //codigo
+    public String name;
+    //para procs/funcs
+    public ArrayList<Symbol> parList;
+    public boolean isVar;
+    public boolean isCompVector;
+    //si es una constante, se puede evaluar en tiempo de compilacion
+    public boolean isConst;
+
+    public Attributes() {
+        type = Symbol.Types.UNDEFINED;
+        parClass = Symbol.ParameterClass.NONE;
+
+        valInt = 0;
+        valBool = false;
+        valString = "";
+
+        code = "";
+        name = "";
+
+        isVar = false;
+        isCompVector = false;
+
+        isConst = false;
+        //COMPLETAR
+    }
+
+    public Attributes clone() {
+    	try {
+    		return (Attributes) super.clone();
+    	}
+    	catch (CloneNotSupportedException e) {
+    		return null; 
+    	}
+    }
+
+    public String toString() {
+        return
+            "type = " + type + "\n" +
+            "parClass = " + parClass + "\n" ;
+            //COMPLETAR
+        
+    }
+
+    public void initialiseParList(){
+        this.parList = new ArrayList<>();
+    }
+}
